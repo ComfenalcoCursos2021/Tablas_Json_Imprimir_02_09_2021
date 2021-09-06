09141d49eb65731713a6a78f22848685fca8d627
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-09-05 21:55:06 -0500

    HTML vinculado con  JSON 05-09-2021

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ addEventListener('DOMContentLoaded', async(e)=>{
     let data = await peticion.json();
     console.log(data);
 
-
+    //Informacion Tipo-De-Factura
     document.querySelector("#tipoDeFactura").insertAdjacentText('afterbegin', data.Informacion['Tipo-De-Factura']);
 
-
+    //Header Logo
     let link = document.createElement('LINK');
     let myLinkObj = {
         rel:"shortcut icon",
@@ -23,6 +23,7 @@ addEventListener('DOMContentLoaded', async(e)=>{
     document.querySelector("#logo").insertAdjacentElement('afterbegin', img);
     document.head.insertAdjacentElement('beforeend', link);
 
+    //Header Empresa
     let headerEmpresa = `
     <strong>${data.Header.Empresa.Nombre}</strong><br>
         ${data.Header.Empresa.Direccion}<br>
@@ -33,15 +34,102 @@ addEventListener('DOMContentLoaded', async(e)=>{
     document.head.insertAdjacentElement('afterbegin',title);
     document.querySelector("#headerEmpresa").insertAdjacentHTML('afterbegin', headerEmpresa);
 
+    //Header Contactos
+    let fragmen = document.createDocumentFragment();
+    let p = document.createElement('P');
+    fragmen.append(p)
+    for(let key in data.Header.Contactos){
+        let texto = document.createTextNode(`${key} :`);
+        fragmen.children[0].appendChild(texto);
+        for(let [id, value] of Object.entries(data.Header.Contactos[key])){
+            let a = document.createElement('A');
+            let br = document.createElement('BR');
+            a.href = value.Valor;
+            a.insertAdjacentText('afterbegin', value.Nombre);
+            fragmen.children[0].appendChild(a);
+            fragmen.children[0].appendChild(br);
+        }
+    }
+    document.querySelector("#headerContactos").insertAdjacentElement('afterbegin', fragmen.children[0]);
+
+
+    let sectionAutorizacionResponsable = `
+    <strong>Facturar a</strong><br>
+    ${data['Section-Autorizacion'].Responsable.Nombre}<br>
+    ${data['Section-Autorizacion'].Responsable.Empresa}<br>
+    ${data['Section-Autorizacion'].Responsable['Dirrecion-Completa']}<br>`;
+    document.querySelector("#sectionAutorizacionResponsable").insertAdjacentHTML('afterbegin', sectionAutorizacionResponsable);
+
+
+    let sectionAutorizacionAutorizacion = `
+    <strong>Aprobado por</strong><br>
+    ${data['Section-Autorizacion'].Autorizacion.Nombre}<br>
+    ${data['Section-Autorizacion'].Autorizacion.Empresa}<br>
+    ${data['Section-Autorizacion'].Autorizacion['Dirrecion-Completa']}<br>`;
+    document.querySelector("#sectionAutorizacionAutorizacion").insertAdjacentHTML('afterbegin', sectionAutorizacionAutorizacion);
+
+
+
+    let ListaProvedores = ``;
+    for(let [id, value] of Object.entries(data['Section-Detalle'].Proveedor)){
+        ListaProvedores += `<tr class="invoice_detail">
+        <td width="20%">${value['N-Vendedor']}</td>
+        <td width="25%" style="text-align:center;">${value['Nombre']}</td>
+        <td width="25%">${value['Orden-Compra']}</td>
+        <td width="30%">${value['Términos-y-condiciones']}</td>
+      </tr>`
+    }
+    document.querySelector("#sectionDetalleProveedor").insertAdjacentHTML('afterbegin', ListaProvedores);
+
+
+    let ListaCompra = "";
+    for(let [id, value] of Object.entries(data['Section-Detalle'].Compra)){
+        ListaCompra += `
+        <tr>
+          <td width='5%'><a class="control removeRow" href="#">x</a> <span contenteditable>${value['N-Vendedor']}</span></td>
+          <td width='5%'><span contenteditable>${value.Codigo}</span></td>
+          <td width='60%'><span contenteditable>${value.Descripcion}</span></td>
+          <td class="amount"><input type="text" value="${value.Cantidad}" /></td>
+          <td class="rate"><input type="text" value="${new Intl.NumberFormat("de-DE").format(value.Precio)}" /></td>
+          <td class="tax taxrelated">${value.Iva}</td>
+          <td class="sum"></td>
+        </tr>`;
+    }
+    document.querySelector("#sectionDetalleCompra").insertAdjacentHTML('afterbegin', ListaCompra);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // Codigo de barras
-    JsBarcode("#barcode", "Hola como estas :V");
+    JsBarcode("#barcode", "Hola como esta");
 
     // Codigo de qr
     qr = new QRious({
         element: document.getElementById('qr-code'),
         size: 200,
-        value: "Miguel Angel"
+        value: "Hola monica"
     });
 
 

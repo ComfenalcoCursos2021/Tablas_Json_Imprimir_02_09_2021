23ec8e2ebbed71efe92937321d2fda1728a28d86
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-09-07 17:07:00 -0500

    Eliminar los item, barras, qr 07 09 2021

--- js/main.js
+++ js/main.js
@@ -25,7 +25,8 @@ addEventListener('DOMContentLoaded', async(e)=>{
 
     //Header Empresa
     let headerEmpresa = `
-    <strong>${data.Header.Empresa.Nombre}</strong><br>
+        <strong>${data.Header.Empresa.Nombre}</strong><br>
+        NIT: ${data.Header.Empresa.NIT}<br>
         ${data.Header.Empresa.Direccion}<br>
         ${data.Header.Empresa.Departamento}<br>`;
     let title = document.createElement('TITLE');
@@ -77,7 +78,7 @@ addEventListener('DOMContentLoaded', async(e)=>{
     for(let [id, value] of Object.entries(data['Section-Detalle'].Proveedor)){
         ListaProvedores += `
         <tr class="invoice_detail">
-            <td width="20%"><a class="control removeRow" href="#">x</a><span contenteditable>${value['N-Vendedor']}</span></td>
+            <td width="20%"><a class="control removeRow" >x</a><span contenteditable>${value['N-Vendedor']}</span></td>
             <td width="22%" style="text-align:center;"><span contenteditable>${value['Nombre']}</span></td>
             <td width="22%"><span contenteditable>${value['Orden-Compra']}</span></td>
             <td width="34%"><span contenteditable>${value['Términos-y-condiciones']}</span></td>
@@ -85,39 +86,81 @@ addEventListener('DOMContentLoaded', async(e)=>{
     }
     document.querySelector("#sectionDetalleProveedor").insertAdjacentHTML('afterbegin', ListaProvedores);
 
+
+
+
     let subTotal = 0;
     let ListaCompra = "";
     for(let [id, value] of Object.entries(data['Section-Detalle'].Compra)){
-        let iva = value.Precio + (value.Precio * (value.Iva / 100));
+        let iva = (value.Precio + (value.Precio * (value.Iva / 100))) * value.Cantidad;
         subTotal += iva;
         ListaCompra += `
         <tr>
-          <td width='5%'><a class="control removeRow" href="#">x</a> <span contenteditable>${value['N-Vendedor']}</span></td>
+          <td width='5%'><a class="control removeRow">x</a> <span contenteditable>${value['N-Vendedor']}</span></td>
           <td width='5%'><span contenteditable>${value.Codigo}</span></td>
-          <td width='60%'><span contenteditable>${value.Descripcion}</span></td>
+          <td width='60%'><textarea rows="1" >${value.Descripcion}</textarea></td>
           <td class="amount"><input type="text" value="${value.Cantidad}" /></td>
-          <td class="rate"><input type="text" value="${new Intl.NumberFormat("de-DE").format(value.Precio)}" /></td>
-          <td class="tax taxrelated">${value.Iva}</td>
-          <td class="sum">${new Intl.NumberFormat("de-DE").format(iva)}</td>
+          <td width="20%" class="rate"><input type="text" value="${new Intl.NumberFormat("de-DE").format(value.Precio)}" /></td>
+          <td width="15%" class="tax taxrelated"><input type="text" value="${value.Iva}" /></td>
+          <td width="10%" class="sum">${new Intl.NumberFormat("de-DE").format(iva)}</td>
         </tr>`;
     }
     document.querySelector("#sectionDetalleCompra").insertAdjacentHTML('afterbegin', ListaCompra);
-
     document.querySelector("#ivaApagar").insertAdjacentHTML('afterbegin', data.Iva);
-
-
     document.querySelector("#totalApagar").insertAdjacentText('afterbegin', new Intl.NumberFormat("de-DE").format(subTotal + (subTotal * (data.Iva / 100))));
 
 
 
 
+    let tbCompras = '#sectionDetalleCompra';
+    document.querySelector(tbCompras).addEventListener("change", (e)=>{
+        console.log(e.target.value);
+        let listaNodos = document.querySelectorAll(`[id="sectionDetalleCompra"] tr td input, .sum`);
+        let valor = [];
+        let subTotal = 0;
+        for(let [id, valu] of Object.entries(listaNodos)){
+            if(valu.nodeName!="TD"){
+                valu.value = new Intl.NumberFormat("de-DE").format(valu.value.replace(/[$.]/g,''));
+                valor.push(parseInt(valu.value.replace(/[$.]/g,'')));
+            }else{
+                let iva = (valor[1] + (valor[1] * (valor[2] / 100))) * valor[0];
+                valu.textContent = new Intl.NumberFormat("de-DE").format(iva);
+                subTotal += iva;
+                console.log(valor);
+                valor = [];
+            }
+        }
+        document.querySelector("#totalApagar").innerHTML = "";
+        document.querySelector("#totalApagar").insertAdjacentText('afterbegin', new Intl.NumberFormat("de-DE").format(subTotal + (subTotal * (data.Iva / 100))));
+    })
+
+    document.querySelector("#FooterMensaje").insertAdjacentText('afterbegin', data.Footer.Mensaje);
 
 
 
 
 
+    document.querySelector("#nuevaFilaCompra").addEventListener("click", (e)=>{
+        let plantilla = `<tr>
+          <td width='5%'><a class="control removeRow">x</a> <span contenteditable>N° Vendedor</span></td>
+          <td width='5%'><span contenteditable>Código</span></td>
+          <td width='60%'><textarea rows="1">Descripción del producto</textarea></td>
+          <td class="amount"><input type="text" value="0"/></td>
+          <td width="20%" class="rate"><input type="text" value="0" /></td>
+          <td width="15%" class="tax taxrelated"><input type="text" value="0" /></td>
+          <td width="10%" class="sum">0</td>
+        </tr>`;
+        document.querySelector("#sectionDetalleCompra").insertAdjacentHTML('beforeend', plantilla);
+        e.preventDefault();
+    })
 
 
+
+    // document.querySelectorAll(`[id="sectionDetalleCompra"] tr td`).addEventListener("click", (e)=>{
+    //     // let listaNodos = document.querySelectorAll(`[id="sectionDetalleCompra"] tr td input, .sum`);
+    //     console.log(e.target);
+    //     e.preventDefault();
+    // })
 
 
 
